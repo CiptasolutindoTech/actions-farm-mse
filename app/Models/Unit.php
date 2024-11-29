132b0a6f528a411f1e1c088ec21ad34b8bd017c0
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-11-29 13:29:21 +0700

    enhance(Unit): add softdelete

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -1,17 +1,20 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Unit extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['id']; // Everything is mass-assignable except for 'id'
     // Allow mass assignment for the 'name' attribute
     protected $fillable = ['name','code'];
 
+    // Kolom untuk soft delete
+    protected $dates = ['deleted_at'];
     // If you're using timestamps (created_at, updated_at), ensure that the $timestamps property is not set to false
     // protected $timestamps = false; // Uncomment this line if you are not using timestamps
 }
