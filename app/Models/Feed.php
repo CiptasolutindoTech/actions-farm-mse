54f8225d11c174714fb27e1244b6bbb5fc742fe7
Meysellin <Meysellinnnn@gmail.com>
2024-12-10 11:11:52 +0700

    enhance(feed)

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -4,9 +4,68 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Feed extends Model
 {
-    /** @use HasFactory<\Database\Factories\FeedFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'feed';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'item_id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the primary key.
+     *
+     * @var string
+     */
+    protected $keyType = 'unsignedBigInteger';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'item_id',
+        'feed_type',
+        'expiration_date',
+        'created_id',
+        'updated_id',
+        'deleted_id',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['expiration_date', 'deleted_at'];
+
+    /**
+     * Define the relationship to the `Item` model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function item()
+    {
+        return $this->belongsTo(Item::class, 'item_id');
+    }
 }
