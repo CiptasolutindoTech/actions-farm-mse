16193d23076c00cd1e6fbe4311e97340dc83d508
Ikhsan <ikhsansetyo05@gmail.com>
2024-12-16 11:36:15 +0700

    enhange(Category): mengubah system menu

--- database/migrations/2023_11_11_045915_system_menu_mapping.php
+++ database/migrations/2023_11_11_045915_system_menu_mapping.php
@@ -24,6 +24,7 @@ return new class extends Migration
              // Insert admin user
             DB::table('system_menu_mapping')->insert([
                 ['user_group_id' => 1,'id_menu' => 1  ],
+                ['user_group_id' => 1,'id_menu' => 10 ],
                 ['user_group_id' => 1,'id_menu' => 2  ],
                 ['user_group_id' => 1,'id_menu' => 21  ],
                 ['user_group_id' => 1,'id_menu' => 3  ],
